ab71eafb2ebe86fbf4ef18a4adf0f81eb7b6d59d
rcarreon <rcarreon.gn@gmail.com>
2017-04-25 18:45:11 -0700

    arreglando print a 5cm x 2.5cm

--- ins.php
+++ ins.php
@@ -985,7 +985,7 @@ if (!empty($_GET['imp_folio'])){
     $result=mysql_query($query,$con2);
     while($dato = mysql_fetch_row($result)){
           
-             echo "<br><div style=\"border:1px solid #545454;width:255px; height:115px;margin:10px; \"><br><div align=center  ><p><img src=barcode/barcode.php?text=".$dato[0]."/></p>";
+             echo "<br><div style=\"border:1px solid #545454;width:189px; height:94.5px;margin:10px; \"><br><div align=center  ><p><img src=barcode/barcode.php?text=".$dato[0]."/></p>";
              echo "<p>$dato[1]</p>";
              echo "<p style=\"font-size:9px;width:260px;\">$dato[2]</p></div>"; 
              echo "</div>";

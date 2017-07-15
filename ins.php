1e29c944098d646e9cc4de153399921d6442e717
rcarreon <rcarreon.gn@gmail.com>
2017-07-15 12:02:41 -0700

    etiquetas otra vez

--- ins.php
+++ ins.php
@@ -1047,7 +1047,6 @@ if (!empty($_GET['imp_folio'])){
               echo "<div class=\"label page-break\"  ><img  align=\"center\" height=\"32px\" width=\"182px\" src=barcode/barcode.php?text=".$dato[0]."/>";
 	      echo "<br><br>";
               echo "<p align=center style=\"font-size:15px; \">$dato[1]</p>";
-	      echo "<br><br>";
               echo "<p align=center style=\"font-size:15px; \">$dato[2]</p>";
               echo "</div>";
               //echo "<div class=\"page-break\"></div>";
